f2aff36b309a3fc128a51fdea3c9b4d3827e4815
ChrisXiao <chris.xiao@hardcoretech.co>
2022-12-09 01:09:40 +0800

    feat(following-app): use table mode

--- following/models.py
+++ following/models.py
@@ -6,6 +6,8 @@ class Follow(models.Model):
     user = models.ForeignKey(User, related_name='follow_user_set', on_delete=models.CASCADE)
     followed = models.ForeignKey(User, related_name='follow_follower_set', on_delete=models.CASCADE)
 
+    unique_together = ['user', 'followed']
+
     def __str__(self):
         return f'{self.user.username} follow {self.followed.username}'
 
