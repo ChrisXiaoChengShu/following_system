c56382dac4782a56c21098298c99a67701402ca0
ChrisXiao <chris.xiao@hardcoretech.co>
2022-12-08 18:06:15 +0800

    migrations(following-app): add follow model

--- following/admin.py
+++ following/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Follow
 
-# Register your models here.
+
+class FollowAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Follow, FollowAdmin)
--- following/models.py
+++ following/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, related_name='follow_user_set', on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='follow_follower_set', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} follow {self.followed.username}'
